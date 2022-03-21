e5904804213f2a96da1990b6f28c3540b31ceded
Ankit Kotnala <31920677+ankitkotnala@users.noreply.github.com>
2022-03-21 21:21:03 +0530

    added databricks provider config and auth

--- db.tf
+++ db.tf
@@ -18,6 +18,16 @@ provider "azurerm" {
   tenant_id = var.tenant_id
 }
 
+  
+  provider "databricks" {
+  host = azurerm_databricks_workspace.this.workspace_url
+  azure_workspace_resource_id = azurerm_databricks_workspace.this.id
+  azure_client_id = var.client_id
+  azure_client_secret = var.client_secret
+  azure_tenant_id = var.tenant_id
+
+}
+  
 variable "region" {
   type = string
   default = "westeurope"
