28143e4ada23bd9c34caa723ac8452ffe87dd119
Ankit Kotnala <31920677+ankitkotnala@users.noreply.github.com>
2022-03-21 20:14:40 +0530

    pipeline variable files

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "client_id"{}
+variable "client_secret"{}
+variable "state_storage_account_name"{}
+variable "state_storage_container_name"{}
+variable "statefilename"{}
+variable "storagekey"{}
+variable "subscription_id"{}
+variable "tenant_id"{}
